ce095f5b0c543a650f9be7ebb08e4f3020796a4a
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2025-12-03 22:59:34 +0000

    created the logic for my insecure payment page

--- car-dealership/app/payment/page.js
+++ car-dealership/app/payment/page.js
@@ -4,6 +4,33 @@ import { useState } from "react";
 
 export default function PaymentPage(){
     const [cardDetails, setCardDetails] = useState({
-        
-    })
+        number: '',
+        expiry: '',
+        cvv: '',
+        name: ''
+
+    });
+
+    const [storedCards, setStoredCards] = useState([]);
+    const [search, setSearch] = useState('');
+
+    //stores card details insecurely
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //storing card in plain text
+        setStoredCards([...storedCards, {...cardDetails, id: Date.now() }]);
+        setCardDetails({number: '', expiry: '', cvv: '', name: ''});
+        alert('Card Stored!')
+    };
+
+    //SQL injection simulation
+    const searchCards = () => {
+        //String concatenation
+        const fakeQuery = `SELECT * FROM cards WHERE number LIKE '%${search}'`;
+        alert(`simulated query: ${fakeQuery} \nTry: ' OR '1'='1' --`);
+    };
+
+    return (
+        <div className=""
+    )
 }
